0bd8319fed6f664c33018733cc16dc30c8482a70
Ahmedabdelaziz77 <zozahmed770@gmail.com>
2024-01-20 23:07:52 +0200

    SpaceX clone only 6 pages

---
+++ SpaceX/js/script.js
@@ -0,0 +1,157 @@
+const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
+
+btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage);
+
+function navToggle() {
+  btn.classList.toggle('open');
+  overlay.classList.toggle('overlay-show');
+  document.body.classList.toggle('stop-scrolling');
+  menu.classList.toggle('show-menu');
+}
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+}
+
+
+
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+      // Get count target
+      const target = +counter.getAttribute('data-target');
+      // Get current counter value
+      const c = +counter.innerText;
+
+      // Create an increment
+      const increment = target / 100;
+
+      // If counter is less than target, add increment
+      if (c < target) {
+        // Round up and set counter value
+        counter.innerText = `${Math.ceil(c + increment)}`;
+
+        setTimeout(updateCounter, target < 20? 75:15);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+function reset() {
+  counters.forEach((counter) => (counter.innerHTML = '0'));
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+/* Login */
+// Create a registration system
+var objPeople = [
+	{
+		username: 'sam',
+		password: 'password25'
+	},
+	{
+		username: 'matt',
+		password: 'password88'
+	},
+	{
+		username: 'chris',
+		password: 'password3'
+	}
+]
+
+// login functionality
+function login() {
+  console.log(objPeople);
+	// retreive data from username and store in username variable
+	var username = document.getElementById('username').value
+	// retreive data from password and store in password variable
+	var password = document.getElementById('password').value
+
+	// loop through all the user pbjects and confrim if the username and password are correct
+	for(var i = 0; i < objPeople.length; i++) {
+		// check to 
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			console.log(username + ' is logged in!!!')
+			// stop the statement if result is found true - this was a return in the video, break is best practice here
+			break
+		} else {
+			// error if username and password don't match
+			console.log('incorrect username or password')
+		}
+	}
+}
+
+// register functionality
+function registerUser() {
+	// store new users username
+	var registerUsername = document.getElementById('newUsername').value
+	// store new users password
+	var registerPassword = document.getElementById('newPassword').value
+	// store new user data in an object
+	var newUser = {
+		username: registerUsername,
+		password: registerPassword
+	}
+	// loop throught people array to see if the username is taken, or password to short
+	for(var i = 0; i < objPeople.length; i++) {
+		// check if new username is equal to any already created usernames
+		if(registerUser == objPeople[i].username) {
+			// alert user that the username is take
+			alert('That username is alreat in user, please choose another')
+			// stop the statement if result is found true
+			break
+		// check if new password is 8 characters or more
+		} else if (registerPassword.length < 8) {
+			// alert user that the password is to short
+			alert('That is to short, include 8 or more characters')
+			// stop the statement if result is found true
+			break
+		}
+	}
+	// push new object to the people array
+	objPeople.push(newUser)
+	// console the updated people array
+	console.log(objPeople)
+}
